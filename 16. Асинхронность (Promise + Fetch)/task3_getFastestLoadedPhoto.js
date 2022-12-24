19b1b979ce14fad8e74a9ea16295a00d1b2edacb
GGulnaz <gilazieva.00@mail.ru>
2022-12-24 19:41:51 +0300

    1941

--- 16. Асинхронность (Promise + Fetch)/task3_getFastestLoadedPhoto.js
+++ 16. Асинхронность (Promise + Fetch)/task3_getFastestLoadedPhoto.js
@@ -1,38 +1,21 @@
 const PHOTOS_URL = "https://jsonplaceholder.typicode.com/photos";
-
 const dataContainer = document.querySelector('#data-container');
 
 const createNewPhoto = (photo) => {
     const newPhoto = document.createElement('li');
+    newPhoto.classList.add('photo-item');
+
     const newPhotoImg = document.createElement('img');
     newPhotoImg.classList.add('photo-item__image');
     newPhotoImg.setAttribute('src', `${photo.url}`);
-    newPhoto.append(newPhotoImg);
+    
     const newPhotoTitle = document.createElement('h3');
     newPhotoTitle.classList.add('photo-item__title');
     newPhotoTitle.textContent = photo.title;
-    newPhoto.prepend(newPhotoTitle);
-    return newPhoto;
-}
 
-const getFastestLoadedPhoto = (ids) => {
-    toggleLoader();
-    const requests = ids.map((id) => fetch(`${PHOTOS_URL}/${id}`));
-    
-    Promise.race(requests)
-        .then((result) => {
-            return result.json();
-        })
-        .then((result) => {
-            const photoHTML = createNewPhoto(result);
-            dataContainer.append(photoHTML);
-        })
-        .catch((error) => {
-            console.log('упс!',error);
-        })
-    .finally(() => {
-        toggleLoader();
-    })
+    newPhoto.append(newPhotoImg);
+    newPhoto.append(newPhotoTitle);
+    return newPhoto;
 }
 
 const toggleLoader = () => {
@@ -44,5 +27,23 @@ const toggleLoader = () => {
         loaderHTML.setAttribute('hidden', '');
     }
 }
+
+const getFastestLoadedPhoto = (ids) => {
+    toggleLoader();
+    Promise.race(ids.map((id) => fetch(`${PHOTOS_URL}/${id}`)))
+        .then((result) => result.json())
+        .then((photo) => {
+            const photoHTML = createNewPhoto(photo);
+            dataContainer.append(photoHTML);
+        })
+        .catch((error) => {
+            console.log('упс!',error);
+        })
+    .finally(() => {
+        toggleLoader();
+    })
+}
+
+
 
 getFastestLoadedPhoto([60, 12, 55, 61, 4, 78, 2, 22]);
