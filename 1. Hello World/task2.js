e835c8a4325c8cc896b5f760d6bedc4a231f0b0f
GGulnaz <gilazieva.00@mail.ru>
2022-12-24 19:13:55 +0300

    1913

---
+++ 1. Hello World/task2.js
@@ -0,0 +1,13 @@
+console.log("запускаем таймер")
+// alert("5")
+// alert("4")
+// alert("3")
+// alert("2")
+// alert("1")
+// alert("0")
+let i = 5;
+while (i>=0) {
+  alert( i );
+  i--;
+}
+console.log("Обратный отсчет закончен")
