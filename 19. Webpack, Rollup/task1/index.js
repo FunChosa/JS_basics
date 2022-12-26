587e57b68d9f46fff4bb83525fc9a9d8a969b793
FunChosa <gilazieva.00@mail.ru>
2022-12-27 00:01:52 +0300

    2356

--- 19. Webpack, Rollup/task1/index.js
+++ 19. Webpack, Rollup/task1/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import './index.css';
+import JS_IMAGE from './assets/js-image.jpg';
+
+console.log("-Hello world-");
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.className = 'js-image';
+jsImageHTML.src = JS_IMAGE;
+
+const pageTitle = document.createElement('h1');
+pageTitle.textContent = "I love JavaScript";
+
+document.body.append(jsImageHTML);
+document.body.append(pageTitle);
--- 19. Webpack, Rollup/task1/rollup.config.js
+++ 19. Webpack, Rollup/task1/rollup.config.js
@@ -1,3 +1,9 @@
+import styles from "rollup-plugin-styles";
+import image from '@rollup/plugin-image';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 export default {
     input: './index.js',
     output: {
@@ -6,5 +12,21 @@ export default {
     },
     watch: {
         exclude: 'node_modules/**'
-      }
+      },
+    plugins: [
+        styles(),
+        image({
+            limit: 100000
+        }
+        ),
+        babel({ babelHelpers: 'bundled',
+        presets: ["@babel/env"] 
+        }),
+        serve({
+        open: true,
+        contentBase: './',
+        port: 8000,
+        }),
+        livereload(),
+    ],
   };
