2e815705c6f0537367d4d4e74772ee4690585661
FunChosa <gilazieva.00@mail.ru>
2022-12-27 01:18:41 +0300

    0112

---
+++ 19. Webpack, Rollup/task2_Webpack/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import JS_IMAGE from './assets/js-image.jpg';
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.src = JS_IMAGE;
+
+document.body.append(jsImageHTML);
+
+console.log('Hello World!');
