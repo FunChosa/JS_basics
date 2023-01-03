56e7699858d7d32e7e4d01bc9195809b21262d21
FunChosa <gilazieva.00@mail.ru>
2023-01-03 22:10:54 +0300

    2210

---
+++ 20. ES6 import export/examle/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+import  App  from './src/modules/app'; //import {имя класса} from 'путь до файла из текущего файла'
+
+
+const app = new App();
+app.run();
+
+console.log('__woiirk__');
