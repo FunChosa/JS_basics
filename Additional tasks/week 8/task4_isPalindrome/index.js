5fd4945eac51c6d93a107487395de055efde556f
FunChosa <gilazieva.00@mail.ru>
2023-01-04 15:34:50 +0300

    1534

--- Additional tasks/week 8/task4_isPalindrome/index.js
+++ Additional tasks/week 8/task4_isPalindrome/index.js
@@ -1,6 +1,6 @@
 function isPalindrome(word) {
     let originalWord = word.split('').join(''); 
-    let reverseWord = word.split('').reverse().join("");
+    let reverseWord = word.split('').reverse().join('');
     return originalWord == reverseWord;
 }
 console.log(isPalindrome('racecar'));// true 
