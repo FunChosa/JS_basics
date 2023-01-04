e52ede7b3d3d976d08e74bc612a3faee4ca6530a
FunChosa <gilazieva.00@mail.ru>
2023-01-04 14:38:24 +0300

    1438

--- 20. ES6 import export/task-2-initial/index.js
+++ 20. ES6 import export/task-2-initial/index.js
@@ -1,1 +1,16 @@
 import './index.css';
+
+// localStorage.clear();
+
+const cookieConsent = document.querySelector(".cookie-consent");
+
+if (localStorage.getItem("cookieButtonResult") === "true"){
+    cookieConsent.classList.add('hide');
+
+} else {
+    cookieConsent.addEventListener('click', () =>  {
+        cookieConsent.classList.add('hide');
+        localStorage.setItem("cookieButtonResult", "true");
+
+    })
+}
