8179351d9a555e465c21d3e7507e911191d5f12f
FunChosa <gilazieva.00@mail.ru>
2023-01-08 21:44:54 +0300

    2144

--- Additional tasks/week 8/task-1(extra)/index.js
+++ Additional tasks/week 8/task-1(extra)/index.js
@@ -1,1 +1,21 @@
 import './index.css';
+
+
+const donatButton = document.querySelector('.donate-form__submit-button');
+donatButton.addEventListener('click', (event) =>  {
+    event.preventDefault();
+    
+    const donat = document.querySelector('.donate-form__donate-input');
+    const totalAmount = document.querySelector('#total-amount');
+    const totalAmountLikeNumber = Number((totalAmount.textContent).replace('$',''));
+    const donatesContainerDonates = document.querySelector('.donates-container__donates');
+
+    totalAmount.textContent = (`${totalAmountLikeNumber + Number(donat.value)}$`);
+    const newDonat = document.createElement('div');
+    newDonat.className = 'donate-item';
+    newDonat.textContent = `${new Date()} - ${donat.value}`
+    donatesContainerDonates.append(newDonat);
+    donat.value = "";
+
+})
+
